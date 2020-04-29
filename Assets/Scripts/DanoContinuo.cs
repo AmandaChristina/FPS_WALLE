e6dffb4b230c23a1731e7779129586fc119f2821
AmandaChristina <amanda.christina.n@gmail.com>
2020-04-29 12:23:20 -0300

    Segunda tentativa

---
+++ Assets/Scripts/DanoContinuo.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DanoContinuo : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public string descricaoAtual = "";
+    
+    public float danoporSegundo;
+    public bool trancado;
+    public bool interpolacao;
+    void Start()
+    {
+        trancado = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!trancado) { 
+            danoporSegundo = 0;
+            Interagivel interagir = GetComponent<Interagivel>();
+            interagir.enabled = true;
+            interagir.descricao = descricaoAtual;
+        }
+
+        if (interpolacao)  GetComponent<Interpolacao>().enabled = true;
+    }
+
+   
+}
---
+++ Assets/Scripts/Jogador.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Jogador : MonoBehaviour
+{
+    public Image cartaoAcesso;
+    public Color cartaoCor;
+    public Slider barraBateria;
+    public float maxBateria;
+    public float bateria;
+    public bool temCartaoacesso;
+
+    void Start()
+    {
+        cartaoCor = cartaoAcesso.color;
+        cartaoCor.a = 0.3f;
+        
+        barraBateria.maxValue = maxBateria;
+        bateria = maxBateria;
+    }
+
+    void Update()
+    {
+        cartaoAcesso.color = cartaoCor;
+        barraBateria.value = bateria;
+
+        if (!temCartaoacesso)
+        {
+            cartaoCor.a = 0.3f;
+            cartaoAcesso.color = cartaoCor;
+        }
+
+    }
+
+    //Só o Jogador tem gerenciamento de Bateria
+    public void Bateria(float baterialAtual)
+    {
+        bateria += baterialAtual;
+        bateria = Mathf.Clamp(bateria, 0f, maxBateria);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "Eletrico")
+        {
+            Vida vida = GetComponent<Vida>();
+            DanoContinuo dano = other.GetComponent<DanoContinuo>();
+            vida.AtualizaVida(dano.danoporSegundo * Time.deltaTime);
+        }
+    }
+
+
+}

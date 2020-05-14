d6c27f206394181a03268ffc766443c18a124f30
AmandaChristina <amanda.christina.n@gmail.com>
2020-05-13 22:59:51 -0300

    imã gigante funcionando
    Só falta algumas questões de Design, como de fato será para desligá-lo

--- Assets/Scripts/Ima.cs
+++ Assets/Scripts/Ima.cs
@@ -1,31 +1,67 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Ima : MonoBehaviour
 {
-    public Transform origem;
-    public GameObject jogadorDirecao;
+    Transform origem;
+    GameObject jogadorDirecao;
+    CharacterController jogadorCC;
+    Vida vidaIma;
+    Canvas vidaUI;
+
+    Vector3 novaPosicaoJogador;
+
     public float alcance;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+
+    bool abduzido;
+
+   
+    void Start(){
+        origem = gameObject.transform;
+        jogadorDirecao = GameObject.Find("Player");
+        jogadorCC = jogadorDirecao.GetComponent<CharacterController>();
+        vidaIma = GetComponentInParent<Vida>();
+        vidaUI = GetComponentInParent<Canvas>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    
+    void Update(){
+        //Tentando deixar a barra de vida na direção do jogador
+        //FALTA PEGAR O COMPONENTE DA FORMA CORRETA
+        //vidaUI.transform.rotation = Quaternion.LookRotation(jogadorDirecao.transform.eulerAngles * Time.deltaTime);
+
+        if(vidaIma.vida == 0) {
+            jogadorCC.enabled = true;
+            if (vidaIma.Morreu()) return; 
+        }
+
         
+
         Vector3 direcao = jogadorDirecao.transform.position - origem.transform.position;
         Ray raio = new Ray(origem.transform.position, direcao);
         RaycastHit hit;
         Debug.DrawRay(raio.origin, raio.direction * alcance);
+        
+        print(" ");
 
         if(Physics.Raycast(raio, out hit, alcance))
         {
-            if (hit.transform.name == "Player") print("Jogador!!!");
+            if (hit.transform.name == "Player")
+            {
+                Abduzido();
+            }
+
         }
 
+    }
+
+    void Abduzido(){
+        
+        jogadorCC.enabled = false;
+        novaPosicaoJogador = Vector3.MoveTowards(jogadorDirecao.transform.position, transform.position, Time.deltaTime * 10);
+        jogadorDirecao.transform.position = novaPosicaoJogador;
+        print("Jogador!!!");
     }
 }

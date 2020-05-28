9bcb7ad1486ef4434fdd225da208c0033c4912a8
AmandaChristina <amanda.christina.n@gmail.com>
2020-05-28 14:04:21 -0300

    Prédio elevador, portões, gerenciamento scene

---
+++ Assets/Scripts/BOSSFight/CenaGerenciador.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CenaGerenciador : MonoBehaviour
+{
+    public GameObject elevador;
+    public GameObject portaElevador;
+    public GameObject portaChefao;
+    public GameObject Auto;
+    float tempo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(GameObject.FindGameObjectWithTag("Inimigo2") == null)
+        {
+            portaElevador.GetComponent<Interpolacao>().enabled = true;
+        }
+
+        if(elevador.transform.localPosition == elevador.GetComponent<Interpolacao>().posicaoFinal)
+        {
+            portaChefao.GetComponent<Interpolacao>().enabled = true;
+        }
+
+        if(portaChefao.transform.localPosition == portaChefao.GetComponent<Interpolacao>().posicaoFinal)
+        {
+            if(tempo < 5f)
+            {
+                tempo += Time.deltaTime;
+            }
+            else Auto.GetComponent<AUTO>().enabled = true;
+        }
+    }
+}
---
+++ Assets/Scripts/BOSSFight/Elevador.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Elevador : MonoBehaviour
+{
+    Interpolacao animacao;
+    Vector3 posicaoInicial;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animacao = GetComponent<Interpolacao>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            animacao.enabled = true;
+        }
+    }
+}
